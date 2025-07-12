6d22611abc71fa25ba670be8922b4d43806f3b0e
Runlin Wang <runlinw2@uci.edu>
2025-07-11 17:50:59 -0700

    Respond to multiple PINGS

--- app/main.py
+++ app/main.py
@@ -8,8 +8,15 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept()
-    connection.sendall(b"+PONG\r\n")
+    client_socket, _ = server_socket.accept()
+    while True:
+        request: bytes = client_socket.recv(512)
+        data: str = request.decode()
+
+        # print(data)
+        if "ping" in data.lower():
+            client_socket.send("+PONG\r\n".encode())
+    
 
 if __name__ == "__main__":
     main()
